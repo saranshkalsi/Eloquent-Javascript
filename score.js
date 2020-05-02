d88f6554dd50298970bbe6c0d9d80b37be01e59b
saransh <saransh.singh14@gmail.com>
2020-05-02 07:35:50 +0530

    Dom manipulation done to create a scorekeeper

---
+++ score.js
@@ -0,0 +1,56 @@
+var p1button = document.querySelector("#p1");
+var p2button = document.querySelector("#p2");
+var p1Display = document.getElementById("p1Display");
+var p2Display = document.querySelector("#p2Display");
+var resetButton = document.getElementById("reset");
+var winningScoreDisplay = document.querySelector("p span");
+var p1Score = 0 ;
+var p2Score = 0 ;
+var winningScore = 5;
+var gameOver =  false;
+var numInput = document.querySelector("input");
+
+
+
+
+p1button.addEventListener("click" , function(){
+  if(!gameOver){
+  p1Score++;
+  if(p1Score == winningScore){
+    p1Display.classList.add("winner");
+    gameOver = true;
+  }
+  p1Display.textContent = p1Score;
+  }
+});
+
+p2button.addEventListener("click" , function(){
+  if(!gameOver){
+  p2Score++;
+  if(p2Score == winningScore){
+    p2Display.classList.add("winner");
+    gameOver = true;
+  }
+  p2Display.textContent = p2Score;
+  }
+});
+
+resetButton.addEventListener("click" , function() {
+reset();
+});
+
+function reset() {
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = p1Score;
+  p2Display.textContent = p2Score;
+  p1Display.classList.remove("winner");
+  p2Display.classList.remove("winner");
+  gameOver = false;
+}
+
+numInput.addEventListener("change" , function() {
+winningScoreDisplay.textContent = this.value;
+winningScore = Number(this.value);
+reset();
+});
